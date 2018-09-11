a5b8b6c32616296b5dda689a4e4f2282e7212cae
hhshhd <41174619+hhshhd@users.noreply.github.com>
2018-09-11 16:03:06 +0800

    Add files via upload

---
+++ IB CS/Hw/乘法口诀表.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 15:49:42 2018
+
+@author: hhshhd
+"""
+start=int(input('number:'))
+x=0
+y=0
+while x!=start:
+    print('\n')
+    x+=1
+    y=0
+    for i in range(x):
+        y+=1
+        print(x,'*',y,'', end='')
+else:
+    print('')
