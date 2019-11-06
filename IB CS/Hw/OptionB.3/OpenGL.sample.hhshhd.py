67fc292640dd45b9e958cc6cd6bc61d5adfa6df3
hhshhd <41174619+hhshhd@users.noreply.github.com>
2019-11-06 10:34:09 +0800

    Add files via upload

---
+++ IB CS/Hw/OptionB.3/OpenGL.sample.hhshhd.py
@@ -0,0 +1,34 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+def draw():
+    glBegin(GL_LINES)
+    glColor4f(1.0,1.0,0.0,0.0)
+    glVertex3f(-0.8,-0.8,0.0)
+    glVertex3f(0.8,0.8,0.0)
+
+    glColor4f(0.0,1.0,1.0,0.0)
+    glVertex3f(-0.8,0.8,0.0)
+    glVertex3f(0.8,-0.8,0.0)
+
+
+    glEnd()
+
+    glBegin(GL_QUADS)
+    glColor4f(1.0, 0.0, 0.0, 1.0)
+    glVertex3f(-0.5, -0.366, -0.5)
+    glColor4f(0.0, 1.0, 0.0, 1.0)       
+    glVertex3f(-0.5, 0.366, -0.5)       
+    glColor4f(0.0, 0.0, 1.0, 1.0)   
+    glVertex3f(0.5, 0.336, -0.5)
+    glColor4f(1.0, 0.0, 1.0, 0.0)   
+    glVertex3f(0.5, -0.336, 0.5) 
+    
+    glEnd()                            
+    
+    glFlush()
+
+if __name__ == "__main__":
+    glutInit()                           
+    glutCreateWindow('Quidam Of OpenGL')
+    glutDisplayFunc(draw)                
+    glutMainLoop()         
